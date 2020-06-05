f74c86d24693ef332efe9112afe5bfe9469514b4
Utkarsh Soni <utkarshsoni12@gmail.com>
2020-06-05 19:37:17 +0530

    Add Scroll Indicator

--- main.js
+++ main.js
@@ -99,3 +99,14 @@ function closeModal(){
 function animateBars(item) {
     item.classList.toggle("change");
 }
+
+window.onscroll = function() {
+    scrollIndicator();
+};
+
+function scrollIndicator() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("scrollIndicatorBar").style.width = scrolled + "%";
+}
